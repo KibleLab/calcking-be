3f2031a1f91abb36c7c272f2bf7d75945b7a146d
RegistryROK <registry.forbiz@gmail.com>
2024-04-10 17:43:12 +0900

    Initial Commit

---
+++ src/main/java/kr/kro/calcking/calckingwebbe/CalckingWebBeApplication.java
@@ -0,0 +1,14 @@
+package kr.kro.calcking.calckingwebbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class CalckingWebBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalckingWebBeApplication.class, args);
+	}
+
+}
