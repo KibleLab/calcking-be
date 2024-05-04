1b1b7a624125b3bfcdbc6a3769275f7fa4272571
RegistryROK <registry.forbiz@gmail.com>
2024-05-04 15:08:04 +0900

    add: UserRepository

---
+++ src/main/java/kr/kro/calcking/calckingwebbe/repositories/UserRepository.java
@@ -0,0 +1,26 @@
+package kr.kro.calcking.calckingwebbe.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import jakarta.transaction.Transactional;
+import kr.kro.calcking.calckingwebbe.entities.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+  @Query(value = "SELECT * FROM user WHERE u_id = :uID", nativeQuery = true)
+  public Optional<UserEntity> findUserByID(String uID);
+
+  @Modifying
+  @Transactional
+  @Query(value = "INSERT INTO user (u_id, u_pw, u_name, u_birth, u_phone, u_email) VALUES (:uID, :uPW, :uName, :uBirth, :uPhone, :uEmail)", nativeQuery = true)
+  public void createUser(String uID, String uPW, String uName, String uBirth, String uPhone, String uEmail);
+
+  @Modifying
+  @Transactional
+  @Query(value = "UPDATE user SET u_token = :uToken WHERE u_id = :uID", nativeQuery = true)
+  public void updateUserToken(String uID, String uToken);
+
+}
