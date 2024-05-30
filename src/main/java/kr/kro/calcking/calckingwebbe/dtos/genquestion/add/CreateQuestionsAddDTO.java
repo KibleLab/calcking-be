2f5a0983ca4a6e277cb55d19e736aed149a1ce64
RegistryROK <registry.forbiz@gmail.com>
2024-05-30 11:22:32 +0900

    add: GenQuestionsAddDTO

---
+++ src/main/java/kr/kro/calcking/calckingwebbe/dtos/genquestion/add/CreateQuestionsAddDTO.java
@@ -0,0 +1,23 @@
+package kr.kro.calcking.calckingwebbe.dtos.genquestion.add;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CreateQuestionsAddDTO {
+  @NotNull(message = "원하시는 문제 수를 입력해주세요.")
+  @Min(value = 8, message = "문제 수는 8개 이상이어야 합니다.")
+  @Max(value = 20, message = "문제 수는 20개 이하여야 합니다.")
+  @JsonProperty("number_of_questions")
+  private int numberOfQuestions;
+
+  @NotNull(message = "올림 횟수를 입력해주세요.")
+  @Min(value = 0, message = "올림 횟수는 0 이상이어야 합니다.")
+  @Max(value = 3, message = "올림 횟수는 3 이하여야 합니다.")
+  @JsonProperty("carry")
+  private int carry;
+}
