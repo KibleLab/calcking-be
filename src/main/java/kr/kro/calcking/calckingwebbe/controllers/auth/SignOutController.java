8912e007fc750542df42b99f1f96b55927d518cc
RegistryROK <registry.forbiz@gmail.com>
2024-06-04 20:54:22 +0900

    fix: remove @ValidateAccessToken

--- src/main/java/kr/kro/calcking/calckingwebbe/controllers/auth/SignOutController.java
+++ src/main/java/kr/kro/calcking/calckingwebbe/controllers/auth/SignOutController.java
@@ -1,19 +1,16 @@
 package kr.kro.calcking.calckingwebbe.controllers.auth;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import kr.kro.calcking.calckingwebbe.annotations.ValidateAccessToken;
 import kr.kro.calcking.calckingwebbe.services.auth.SignOutService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.Map;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class SignOutController {
   private final SignOutService signOutService;
 
-  @ValidateAccessToken
   @PostMapping
   public ResponseEntity<Map<String, Object>> signOut(
       HttpServletRequest request, HttpServletResponse response) {
