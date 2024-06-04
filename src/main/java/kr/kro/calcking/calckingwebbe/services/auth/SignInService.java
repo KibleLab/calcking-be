9711b65e1c1e51ad000553401b3f9b8bd6bfce1b
RegistryROK <registry.forbiz@gmail.com>
2024-06-04 20:53:05 +0900

    fix: reflect changes
    reflect TokenRepository, TokenProvider

--- src/main/java/kr/kro/calcking/calckingwebbe/services/auth/SignInService.java
+++ src/main/java/kr/kro/calcking/calckingwebbe/services/auth/SignInService.java
@@ -1,30 +1,66 @@
 package kr.kro.calcking.calckingwebbe.services.auth;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-
-import jakarta.servlet.http.HttpServletResponse;
-import kr.kro.calcking.calckingwebbe.dtos.auth.ReadUserDTO;
-import kr.kro.calcking.calckingwebbe.entities.UserEntity;
-import kr.kro.calcking.calckingwebbe.providers.CookieProvider;
-import kr.kro.calcking.calckingwebbe.providers.JWTProvider;
-import kr.kro.calcking.calckingwebbe.repositories.UserRepository;
-import kr.kro.calcking.calckingwebbe.repositories.UserTokenRepository;
-import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import jakarta.servlet.http.HttpServletResponse;
+import kr.kro.calcking.calckingwebbe.dtos.auth.ReadUserDTO;
+import kr.kro.calcking.calckingwebbe.entities.UserEntity;
+import kr.kro.calcking.calckingwebbe.providers.CookieProvider;
+import kr.kro.calcking.calckingwebbe.providers.TokenProvider;
+import kr.kro.calcking.calckingwebbe.repositories.TokenRepository;
+import kr.kro.calcking.calckingwebbe.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
 public class SignInService {
   private final UserRepository userRepository;
-  private final UserTokenRepository userTokenRepository;
-  private final JWTProvider jwtProvider;
+  private final TokenRepository tokenRepository;
   private final CookieProvider cookieProvider;
+  private final TokenProvider tokenProvider;
+
+  // POST (/sign-in)
+  public ResponseEntity<Map<String, Object>> signIn(ReadUserDTO readUserDTO, HttpServletResponse response) {
+    // ID 및 PW 검증
+    Optional<UserEntity> user = userRepository.readUserByUID(readUserDTO.getUID());
+    if (user.isEmpty())
+      return responseMap("존재하지 않는 사용자 입니다!", HttpStatus.BAD_REQUEST);
+    else if (!isPasswordMatch(readUserDTO.getUPW(), user.get().getUPW()))
+      return responseMap("비밀번호가 일치하지 않습니다!", HttpStatus.BAD_REQUEST);
+
+    // Token Payload 생성
+    Map<String, Object> payload = new HashMap<>();
+    payload.put("ID", user.get().getUID());
+
+    // RefreshToken 발급
+    Date refreshTokenExpireAt = new Date(System.currentTimeMillis() + (1000 * 60 * 60 * 24 * 15));
+    Optional<String> refreshToken = tokenProvider.issueToken(payload, refreshTokenExpireAt);
+    if (refreshToken.isEmpty())
+      return responseMap("토큰 발급 실패!", HttpStatus.INTERNAL_SERVER_ERROR);
+    else {
+      tokenRepository.createToken(refreshToken.get(), refreshTokenExpireAt, user.get().getUID());
+      cookieProvider.createCookie("refresh_token", refreshToken.get(), 60 * 60 * 24 * 15, response);
+    }
+
+    // AccessToken 발급
+    Date accessTokenExpireAt = new Date(System.currentTimeMillis() + (1000 * 60 * 15));
+    Optional<String> accessToken = tokenProvider.issueToken(payload, accessTokenExpireAt);
+    if (accessToken.isEmpty())
+      return responseMap("토큰 발급 실패!", HttpStatus.INTERNAL_SERVER_ERROR);
+    else
+      response.addHeader("Authorization", "Bearer " + accessToken.get().toString());
+
+    // 성공 응답
+    return responseMap("로그인 성공!", HttpStatus.OK);
+  }
 
   // PW 일치 여부 확인 은닉 메서드
   private boolean isPasswordMatch(String rawPW, String encodedPW) {
@@ -32,35 +68,11 @@ public class SignInService {
     return bCryptPasswordEncoder.matches(rawPW, encodedPW);
   }
 
-  // POST (/sign-in)
-  public ResponseEntity<Map<String, Object>> signIn(ReadUserDTO readUserDTO, HttpServletResponse response) {
+  // JSON 응답 생성 은닉 메서드
+  private ResponseEntity<Map<String, Object>> responseMap(String message, HttpStatus status) {
     Map<String, Object> responseMap = new HashMap<>();
-    Optional<UserEntity> user = userRepository.readUserByUID(readUserDTO.getUID());
-
-    // ID 및 PW 검증 로직
-    if (user.isEmpty()) {
-      responseMap.put("message", "ID가 잘못되었거나 존재하지 않습니다!");
-      responseMap.put("status", String.valueOf(HttpStatus.BAD_REQUEST));
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMap);
-    } else if (!isPasswordMatch(readUserDTO.getUPW(), user.get().getUPW())) {
-      responseMap.put("message", "비밀번호가 잘못되었습니다!");
-      responseMap.put("status", String.valueOf(HttpStatus.BAD_REQUEST));
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMap);
-    }
-
-    // RefreshToken 발급 로직
-    String refreshToken = jwtProvider.issueRefreshToken(user.get().getUID());
-    userTokenRepository.deleteUserTokenByUID(user.get().getUID());
-    userTokenRepository.createUserToken(user.get().getUID(), refreshToken);
-    cookieProvider.createCookie("refresh_token", refreshToken, 60 * 60 * 24 * 15, response);
-
-    // AccessToken 발급 로직
-    String accessToken = jwtProvider.issueAccessToken(user.get().getUID());
-    response.addHeader("Authorization", "Bearer " + accessToken);
-
-    // JSON 응답 로직
-    responseMap.put("message", "로그인 성공!");
-    responseMap.put("status", String.valueOf(HttpStatus.OK));
-    return ResponseEntity.status(HttpStatus.OK).body(responseMap);
+    responseMap.put("message", message);
+    responseMap.put("status", String.valueOf(status));
+    return ResponseEntity.status(status).body(responseMap);
   }
 }
--- src/main/java/kr/kro/calcking/calckingwebbe/services/auth/SignOutService.java
+++ src/main/java/kr/kro/calcking/calckingwebbe/services/auth/SignOutService.java
@@ -11,32 +11,34 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.kro.calcking.calckingwebbe.providers.CookieProvider;
-import kr.kro.calcking.calckingwebbe.providers.JWTProvider;
-import kr.kro.calcking.calckingwebbe.repositories.UserTokenRepository;
+import kr.kro.calcking.calckingwebbe.repositories.TokenRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class SignOutService {
-  private final UserTokenRepository userTokenRepository;
+  private final TokenRepository tokenRepository;
   private final CookieProvider cookieProvider;
-  private final JWTProvider jwtProvider;
 
   // POST (/sign-out)
-  public ResponseEntity<Map<String, Object>> signOut(
-      HttpServletRequest request, HttpServletResponse response) {
-    Map<String, Object> responseMap = new HashMap<>();
-
-    // 로그아웃 로직
+  public ResponseEntity<Map<String, Object>> signOut(HttpServletRequest request, HttpServletResponse response) {
+    // 로그아웃
     Cookie refreshCookie = cookieProvider.getCookie("refresh_token", request);
-    userTokenRepository
-        .deleteUserTokenByUID(
-            jwtProvider.getUIDFromAccessToken(request.getHeader("Authorization").substring(7)));
-    cookieProvider.deleteCookie(refreshCookie, response);
+    String refreshToken = cookieProvider.getTokenFromCookie(refreshCookie);
+    if (refreshCookie != null) {
+      cookieProvider.deleteCookie(refreshCookie, response);
+      tokenRepository.deleteToken(refreshToken);
+    }
 
-    // JSON 응답 로직
-    responseMap.put("message", "로그아웃 성공!");
-    responseMap.put("status", String.valueOf(HttpStatus.OK));
-    return ResponseEntity.status(HttpStatus.OK).body(responseMap);
+    // 성공 응답
+    return responseMap("로그아웃 성공!", HttpStatus.OK);
+  }
+
+  // JSON 응답 생성 은닉 메서드
+  private ResponseEntity<Map<String, Object>> responseMap(String message, HttpStatus status) {
+    Map<String, Object> responseMap = new HashMap<>();
+    responseMap.put("message", message);
+    responseMap.put("status", String.valueOf(status));
+    return ResponseEntity.status(status).body(responseMap);
   }
 }
