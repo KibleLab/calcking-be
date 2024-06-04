823d31bcc74cf5efbe77d1816fc3b87467865c9a
RegistryROK <registry.forbiz@gmail.com>
2024-06-04 18:45:58 +0900

    fix: reflect UserRoleEntity

--- src/main/java/kr/kro/calcking/calckingwebbe/repositories/UserRepository.java
+++ src/main/java/kr/kro/calcking/calckingwebbe/repositories/UserRepository.java
@@ -13,12 +13,16 @@ public interface UserRepository extends JpaRepository<UserEntity, String> {
   // Create
   @Modifying
   @Transactional
-  @Query(value = "INSERT INTO user (u_id, u_pw, u_name, u_birth, u_phone, u_email) VALUES (:uID, :uPW, :uName, :uBirth, :uPhone, :uEmail)", nativeQuery = true)
-  public void createUser(String uID, String uPW, String uName, String uBirth, String uPhone, String uEmail);
+  @Query(value = "INSERT INTO user (u_id, u_pw, u_name, u_birth, u_phone, u_email, u_role_id) VALUES (:uID, :uPW, :uName, :uBirth, :uPhone, :uEmail, :uRoleID)", nativeQuery = true)
+  public void createUser(String uID, String uPW, String uName, String uBirth, String uPhone, String uEmail,
+      int uRoleID);
 
   // Read
   @Query(value = "SELECT * FROM user WHERE u_id = :uID", nativeQuery = true)
   public Optional<UserEntity> readUserByUID(String uID);
+
+  @Query(value = "SELECT * FROM user WHERE u_email = :uEmail", nativeQuery = true)
+  public Optional<UserEntity> readUserByUEmail(String uEmail);
 
   // Delete
   @Modifying
